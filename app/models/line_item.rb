b9e1890939fa991ed71309316ef3ffee50f13468
Sebastien Grima <sebastien.grima@gmail.com>
2021-02-03 15:35:37 +0000

    changes post head issue

---
+++ app/models/line_item.rb
@@ -0,0 +1,9 @@
+class LineItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :cart
+  belongs_to :order, optional: true
+
+  def total_price
+    self.quantity.to_s.to_d * self.product.price.to_s.to_d
+  end
+end
