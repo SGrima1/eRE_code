e3ab6a8489c252dd19d5f793c7feac08dda2d294
Sebastien Grima <sebastien.grima@gmail.com>
2021-02-09 11:41:13 +0000

    added in user logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_161248) do
+ActiveRecord::Schema.define(version: 2021_02_09_112658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 2021_02_05_161248) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "sku"
     t.integer "price_cents", default: 0, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,9 +91,11 @@ ActiveRecord::Schema.define(version: 2021_02_05_161248) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "users"
 end
